120db57c4f67c0a212aa3cb69bfc93e833241ddb
KaioAntonio <kaioaar@gmail.com>
2021-10-04 17:51:55 -0300

    Projeto criado

---
+++ routes.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+
+from main import insertUsuario
+
+app = Flask("Youtube")
+
+@app.route("/olamundo", methods=["GET"])
+def olaMundo():
+    return {"Ola": "mundo"}
+
+
+@app.route("/usuario", methods=["GET"])
+def mostrarUsuario():
+    return cadastraUsuario
+
+@app.route("/cadastra/usuario", methods=["POST"])
+def cadastraUsuario():
+
+    body = request.get_json()
+
+    if("nome" not in body):
+        return {"status": 400,"mensagem": "O parametro nome é obrigatorio"}
+
+    if ("email" not in body):
+        return {"status": 400, "mensagem": "O parametro email é obrigatorio"}
+
+    if ("senha" not in body):
+        return {"status": 400, "mensagem": "O parametro senha é obrigatorio"}
+
+    usuario = insertUsuario(body["nome"], body["email"], body["senha"])
+
+    return geraResponse(200, "Usuario criado", "user",usuario)
+
+def geraResponse(status, mensagem, nome_do_conteudo, conteudo):
+    response = {}
+    response["status"] = status
+    response["mensagem"] = mensagem
+
+    if(nome_do_conteudo and conteudo):
+        response[nome_do_conteudo] = conteudo
+
+    return response
+
+app.run()
